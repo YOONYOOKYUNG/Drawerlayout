79763520edd5b1f04b8158f235645f9bc92b402f
YOONYOOKYUNG <kyleykyoon@gmail.com>
2020-09-04 13:07:03 +0900

    update 5 - combine

---
+++ app/src/main/java/com/cookandroid/drawerlayout/ListViewItem.java
@@ -0,0 +1,20 @@
+package com.cookandroid.drawerlayout;
+
+public class ListViewItem {
+    String name;
+    Boolean check;
+
+    public String getName() {
+        return name;
+    }
+    public static Boolean getCheck() {
+        return true;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setCheck(Boolean check){
+        this.check = check;
+    }
+}
