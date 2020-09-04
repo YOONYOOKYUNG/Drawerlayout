4463016e015b3c123b8505b9e14b1b956da78d2b
YOONYOOKYUNG <kyleykyoon@gmail.com>
2020-09-04 14:32:11 +0900

    update 6 - combine

--- app/src/main/java/com/cookandroid/drawerlayout/AlarmActivity.java
+++ app/src/main/java/com/cookandroid/drawerlayout/AlarmActivity.java
@@ -1,21 +1,37 @@
 package com.cookandroid.drawerlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
 import java.util.Calendar;
 
 public class AlarmActivity extends AppCompatActivity {
 
     TextView st_time,st_time2;
     ImageView set_alarm, fin_alarm, backarrow;
+    NotificationManager nm;
+    Notification.Builder builder;
+    PendingIntent fpi;
+    Intent push;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +41,38 @@ public class AlarmActivity extends AppCompatActivity {
         st_time = findViewById(R.id.st_time);
         st_time2 = findViewById(R.id.st_time2);
 
-        Switch switchbtn = findViewById(R.id.sb_use_listener);
+        final Switch switchbtn = findViewById(R.id.sb_use_listener);
+        builder = new Notification.Builder(this);
+        builder.setSmallIcon(R.drawable.alaram);
+        builder.setTicker("waf");
+        builder.setWhen(System.currentTimeMillis());
+        builder.setContentTitle("알림 설정");
+        builder.setContentText("푸쉬 알림이 커졌습니다.");
+        builder.setAutoCancel(true);
+        builder.setPriority(Notification.PRIORITY_MAX);
+
+        nm = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        push = new Intent();
+        push.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        push.setClass(this, AlarmActivity.class);
+
+        fpi = PendingIntent.getActivity(this, 0, push,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+
+        switchbtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(switchbtn.isChecked() == true){
+                    createNotifi();
+                    nm.notify(123456, builder.build());
+                    Toast.makeText(getApplicationContext(),"푸쉬 알림이 켜졌습니다.",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    removeNotifi();
+                    Toast.makeText(getApplicationContext(),"푸쉬 알림이 꺼졌습니다.",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
         st_time.setOnClickListener(new View.OnClickListener() {
@@ -99,6 +146,31 @@ public class AlarmActivity extends AppCompatActivity {
     @Override
     public void onBackPressed(){
         super.onBackPressed();
+    }
+    //알림 켰을때
+    private void createNotifi(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "default");
+
+        builder.setSmallIcon(R.drawable.alaram);
+        builder.setContentTitle("WAF");
+        builder.setWhen(System.currentTimeMillis());
+        builder.setContentText("푸쉬 알림이 켜졌습니다.");
+        builder.setColor(Color.WHITE);
+        //사용자가 알림 클릭시 삭제
+        builder.setAutoCancel(true);
+        //우선순위 부여
+        builder.setPriority(Notification.PRIORITY_MAX);
+        //알림 표시
+        NotificationManager notificationManager = (NotificationManager)this.getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            notificationManager.createNotificationChannel(new NotificationChannel
+                    ("default", "",NotificationManager.IMPORTANCE_DEFAULT));
+        }
+        notificationManager.notify(1,builder.build());
+    }
+    //알림 껐을때
+    private void removeNotifi(){
+        NotificationManagerCompat.from(this).cancel(1);
     }
 }
 
--- app/src/main/java/com/cookandroid/drawerlayout/ConnpopupActivity.java
+++ app/src/main/java/com/cookandroid/drawerlayout/ConnpopupActivity.java
@@ -2,13 +2,18 @@ package com.cookandroid.drawerlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class ConnpopupActivity extends AppCompatActivity {
     Button btnok, btnend;
+    BluetoothAdapter myBluetoothAdapter;
+    Intent btEnableIntent;
+    int REQUEST_ENABLE_BT = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,13 +26,33 @@ public class ConnpopupActivity extends AppCompatActivity {
         btnok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (myBluetoothAdapter == null) {
+                    //블루투스가 지원이 되지 않는 기기 ->나중에 어플 종료로 이어짐
+                    Toast.makeText(getApplicationContext(), "블루투스를 지원하지 않는 기기입니다.", Toast.LENGTH_SHORT).show();
+                } else {
+                    //블루투스 되는 기기이다.
+                    //그렇다면 지금 현재 블루투스 기능이 켜져 있는지 체크 해야 한다.
+                    if (!myBluetoothAdapter.isEnabled()) {
+                        //false이면
+                        //블루투스 꺼져있는상태 -> 간단한 인텐드 이용하여 블루투스 켬.
+                        btEnableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        startActivityForResult(btEnableIntent, REQUEST_ENABLE_BT);
+                        //위 결과값을 받아서 처리 한다.
+                    }
+                }
+
                 Intent intent = new Intent(ConnpopupActivity.this, MainActivity.class);
                 startActivity(intent);
+
             }
         });
+
         btnend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                moveTaskToBack(true);				// 태스크를 백그라운드로 이동
+                finishAndRemoveTask();						// 액티비티 종료 + 태스크 리스트에서 지우기
+                android.os.Process.killProcess(android.os.Process.myPid());
                 finish();
             }
         });
