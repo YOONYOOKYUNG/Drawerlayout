f82a5ed81b99626f3a33b59213d29888560655ac
YOONYOOKYUNG <kyleykyoon@gmail.com>
2020-09-08 16:10:09 +0900

    일부 파일 합침, 습도 팝업 추가

---
+++ app/src/main/java/com/cookandroid/windowairfresh/Suchi.java
@@ -0,0 +1,16 @@
+package com.cookandroid.windowairfresh;
+
+public class Suchi {
+    String suchi, suchig, suchis, suchib;
+
+    public String getSuchi() { return  getSuchi();}
+    public String getSuchig(){ return getSuchig(); }
+    public String getSuchis(){ return getSuchis(); }
+    public String getSuchib(){ return getSuchib(); }
+
+    public void setSuchi(String suchi){this.suchi = suchi;}
+    public void setSuchig(String suchig){ this.suchig = suchig; }
+    public void setSuchis(String suchis){ this.suchis = suchis; }
+    public void setSuchib(String suchib){ this.suchib = suchib; }
+
+}
