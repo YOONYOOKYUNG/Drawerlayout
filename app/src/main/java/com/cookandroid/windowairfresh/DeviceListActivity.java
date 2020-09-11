ddf291c3b9713574c922728e2e3035a90567b716
yuriseo1998 <yuri_seo1998@naver.com>
2020-09-11 19:37:49 +0900

    블루투스 목록 완성
    <확인을 누를 시>
    1. 블루투스 허용
    2. 블루투스 목록창으로 넘어감
    
    연결된 기기, 연결 가능한 기기 둘 다 뜸.
    블루투스 연결시 현재 메인화면으로 이동

--- app/src/main/java/com/cookandroid/windowairfresh/DeviceListActivity.java
+++ app/src/main/java/com/cookandroid/windowairfresh/DeviceListActivity.java
@@ -15,61 +15,87 @@ import java.util.ArrayList;
 
 /**
  * Device list.
- *
+ * 
  * @author Lorensius W. L. T <lorenz@londatiga.net>
  *
  */
 public class DeviceListActivity extends Activity {
-    private ListView mListView;
-    private DeviceListAdapter mAdapter;
-    private ArrayList<BluetoothDevice> mDeviceList;
+	private ListView mListView, mListView2;
+	private DeviceListAdapter mAdapter,mAdapter2;
+	private ArrayList<BluetoothDevice> mDeviceList,mDeviceList2;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_paired_devices);
+		//어느 페이지의 레이아웃인가요?
+		mDeviceList		= getIntent().getExtras().getParcelableArrayList("device.list");
+		mDeviceList2		= getIntent().getExtras().getParcelableArrayList("device.list2");
+		mListView		= (ListView) findViewById(R.id.lv_paired);
+		mListView2		= (ListView) findViewById(R.id.lv_paired2);
+		mAdapter		= new DeviceListAdapter(this);
+		mAdapter2		= new DeviceListAdapter(this);
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+		mAdapter.setData(mDeviceList);
+		mAdapter2.setData(mDeviceList2);
 
-        setContentView(R.layout.activity_paired_devices);
+		mAdapter.setListener(new DeviceListAdapter.OnPairButtonClickListener() {
+			@Override
+			public void onPairButtonClick(int position) {
+				//버튼을 클릭하면
+				BluetoothDevice device = mDeviceList.get(position);
+				if (device.getBondState() == BluetoothDevice.BOND_BONDED) {
+					//BOND_BONDED : 페어링 됐으면
+					unpairDevice(device);
+					//페어링 해제
+				} else {
+					showToast("연결중...");
+					pairDevice(device); //페어링
+				}
+			}
+		});
 
-        mDeviceList		= getIntent().getExtras().getParcelableArrayList("device.list");
+		mListView.setAdapter(mAdapter);
 
-        mListView		= (ListView) findViewById(R.id.lv_paired);
+		mAdapter2.setListener(new DeviceListAdapter.OnPairButtonClickListener() {
+			@Override
+			public void onPairButtonClick(int position) {
+				//버튼을 클릭하면
+				BluetoothDevice device = mDeviceList2.get(position);
+				if (device.getBondState() == BluetoothDevice.BOND_BONDED) {
+					//BOND_BONDED : 페어링 됐으면
+					unpairDevice(device);
+					//페어링 해제
+				} else {
+					showToast("연결중...");
+					pairDevice(device); //페어링
+				}
+			}
+		});
 
-        mAdapter		= new DeviceListAdapter(this);
-
-        mAdapter.setData(mDeviceList);
-        mAdapter.setListener(new DeviceListAdapter.OnPairButtonClickListener() {
-            @Override
-            public void onPairButtonClick(int position) {
-                BluetoothDevice device = mDeviceList.get(position);
-
-                if (device.getBondState() == BluetoothDevice.BOND_BONDED) {
-                    unpairDevice(device);
-                } else {
-                    showToast("Pairing...");
-
-                    pairDevice(device);
-                }
-            }
-        });
-
-        mListView.setAdapter(mAdapter);
-
-        registerReceiver(mPairReceiver, new IntentFilter(BluetoothDevice.ACTION_BOND_STATE_CHANGED));
-    }
-
-    @Override
-    public void onDestroy() {
-        unregisterReceiver(mPairReceiver);
-
-        super.onDestroy();
-    }
+		mListView2.setAdapter(mAdapter2);
 
 
-    private void showToast(String message) {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-    }
 
+
+		registerReceiver(mPairReceiver, new IntentFilter(BluetoothDevice.ACTION_BOND_STATE_CHANGED)); 
+	}
+	
+	@Override
+	public void onDestroy() {
+		unregisterReceiver(mPairReceiver);
+		
+		super.onDestroy();
+	}
+	
+	
+	private void showToast(String message) {
+		Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+	}
+	
     private void pairDevice(BluetoothDevice device) {
+		//선택한 디바이스 페어링 요청
         try {
             Method method = device.getClass().getMethod("createBond", (Class[]) null);
             method.invoke(device, (Object[]) null);
@@ -79,31 +105,36 @@ public class DeviceListActivity extends Activity {
     }
 
     private void unpairDevice(BluetoothDevice device) {
+		//선택한 디바이스 언페어링 요청
         try {
             Method method = device.getClass().getMethod("removeBond", (Class[]) null);
             method.invoke(device, (Object[]) null);
-
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
+    
     private final BroadcastReceiver mPairReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-
-            if (BluetoothDevice.ACTION_BOND_STATE_CHANGED.equals(action)) {
-                final int state 		= intent.getIntExtra(BluetoothDevice.EXTRA_BOND_STATE, BluetoothDevice.ERROR);
-                final int prevState	= intent.getIntExtra(BluetoothDevice.EXTRA_PREVIOUS_BOND_STATE, BluetoothDevice.ERROR);
-
-                if (state == BluetoothDevice.BOND_BONDED && prevState == BluetoothDevice.BOND_BONDING) {
-                    showToast("Paired");
-                } else if (state == BluetoothDevice.BOND_NONE && prevState == BluetoothDevice.BOND_BONDED){
-                    showToast("Unpaired");
-                }
-
-                mAdapter.notifyDataSetChanged();
-            }
-        }
-    };
+	    public void onReceive(Context context, Intent intent) {
+	        String action = intent.getAction();
+	        
+	        if (BluetoothDevice.ACTION_BOND_STATE_CHANGED.equals(action)) {
+	        	//페어링 상태가 변경되면 페어링상태 업데이트
+	        	 final int state 		= intent.getIntExtra(BluetoothDevice.EXTRA_BOND_STATE, BluetoothDevice.ERROR);
+	        	 final int prevState	= intent.getIntExtra(BluetoothDevice.EXTRA_PREVIOUS_BOND_STATE, BluetoothDevice.ERROR);
+	        	 
+	        	 if (state == BluetoothDevice.BOND_BONDED && prevState == BluetoothDevice.BOND_BONDING) {
+	        		 showToast("연결됨");
+					 Intent changeintent = new Intent(DeviceListActivity.this,MainActivity.class);
+					 startActivity(changeintent);
+	        	 } else if (state == BluetoothDevice.BOND_NONE && prevState == BluetoothDevice.BOND_BONDED){
+	        		 showToast("연결해제됨");
+	        	 }
+	        	 
+	        	 mAdapter.notifyDataSetChanged();
+	        	 //어댑터값 갱신
+	        }
+	    }
+	};
 }
