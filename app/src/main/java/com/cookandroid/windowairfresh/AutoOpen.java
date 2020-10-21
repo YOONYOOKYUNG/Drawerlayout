699ba01875be0f97c499db2b2ed38f1cf974343c
yuriseo1998 <yuri_seo1998@naver.com>
2020-10-21 16:30:37 +0900

    10/21[서유리]- 자동개폐 완벽!!

--- app/src/main/java/com/cookandroid/windowairfresh/AutoOpen.java
+++ app/src/main/java/com/cookandroid/windowairfresh/AutoOpen.java
@@ -5,30 +5,28 @@ import android.content.SharedPreferences;
 import android.os.Message;
 import android.util.Log;
 
-import java.util.ArrayList;
-
 public class AutoOpen extends Thread {
     WindowListAdapter adapter = new WindowListAdapter();
 
     public void run() {
-        SharedPreferences sf = (MainActivity.mContext).getSharedPreferences("autoset", 0);
-        int hottemp =  Integer.parseInt(sf.getString("high_temp","30"));
-        int coldtemp= Integer.parseInt(sf.getString("low_temp","0"));
-        int comparedust = Integer.parseInt(sf.getString("compare_dust","20"));
-        Boolean modestate = sf.getBoolean("modestate", false);
-        float insidedust=((MainActivity)MainActivity.mContext).insidedust;
-        float outsidedust=((MainActivity)MainActivity.mContext).outsidedust;
-        float outsidetemp=((MainActivity)MainActivity.mContext).outsidetemp;
-        int outsiderain=((MainActivity)MainActivity.mContext).outsiderain;
         DatabaseManager databaseManager = DatabaseManager.getInstance(MainActivity.mContext);
-        adapter = new WindowListAdapter();
-        adapter.setDatabaseManager(databaseManager);
         while(true) {
+            SharedPreferences sf = (MainActivity.mContext).getSharedPreferences("autoset", 0);
+            int hottemp =  Integer.parseInt(sf.getString("high_temp","30"));
+            int coldtemp= Integer.parseInt(sf.getString("low_temp","0"));
+            int comparedust = Integer.parseInt(sf.getString("compare_dust","20"));
+            Boolean modestate = sf.getBoolean("modestate", false);
+            float insidedust=((MainActivity)MainActivity.mContext).insidedust;
+            float outsidedust=((MainActivity)MainActivity.mContext).outsidedust;
+            float outsidetemp=((MainActivity)MainActivity.mContext).outsidetemp;
+            int outsiderain=((MainActivity)MainActivity.mContext).outsiderain;
+            adapter = new WindowListAdapter();
+            adapter.setDatabaseManager(databaseManager);
             adapter.initialiseList();
-            float dustresult = outsidedust - insidedust;
+            float dustresult = outsidedust-insidedust;
             int windownumber = adapter.getCount();
             if (modestate){
-            if (outsiderain == 0 && coldtemp < outsidetemp && outsidetemp < hottemp && dustresult < -comparedust) {
+            if (outsiderain == 0 && coldtemp<outsidetemp && outsidetemp< hottemp && dustresult+comparedust<0) {
                 Log.d("자동모드", "자동모드:창문 열었어요");
                 boolean windowsOpened = false;
                 for (int i = 0; i < windownumber; i++) {
