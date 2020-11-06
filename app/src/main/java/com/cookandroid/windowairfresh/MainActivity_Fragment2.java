591c6e1c650ba8daf1b37a430a4493db6a5c6167
kgy_waf <37450307+dkfjladkfwpe@users.noreply.github.com>
2020-11-07 00:46:41 +0900

    11/07[김가연] 알림 수정

--- app/src/main/java/com/cookandroid/windowairfresh/MainActivity_Fragment2.java
+++ app/src/main/java/com/cookandroid/windowairfresh/MainActivity_Fragment2.java
@@ -28,7 +28,6 @@ public class MainActivity_Fragment2 extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
