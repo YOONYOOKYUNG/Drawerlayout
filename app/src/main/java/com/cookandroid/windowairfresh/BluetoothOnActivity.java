8e2feadbe506d866105615df474d7ab879668202
gyungwonkim <jy_gw0926@naver.com>
2020-11-25 03:45:11 +0900

    11/25[김경원]- 블루투스 연결 페이지 넘김 수정

--- app/src/main/java/com/cookandroid/windowairfresh/BluetoothOnActivity.java
+++ app/src/main/java/com/cookandroid/windowairfresh/BluetoothOnActivity.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -56,12 +57,22 @@ public class BluetoothOnActivity extends AppCompatActivity {
                     if (!mBluetoothAdapter.isEnabled()) {
                         //false이면
                         //블루투스 꺼져있는상태 -> 간단한 인텐드 이용하여 블루투스 켬.
-                        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                        startActivity(intent);
+
+                        //저장된 주소 확인
+                        SharedPreferences pf = getSharedPreferences("address", MODE_PRIVATE);
+                        String shared_address = pf.getString("addr0", "");
+
+                        if (shared_address.isEmpty()) {//저장된 주소가 비어있을 시
+                            startActivity(new Intent(BluetoothOnActivity.this, AddressActivity.class));
+                            //액티비티 전환 시 애니메이션 효과
+                            overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+                            finish();
+                        } else {//저장된 주소가 있을 시
+                            startActivity(new Intent(BluetoothOnActivity.this, MainActivity.class));
+                            overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+                            finish();
+                        }
                     }
-                    Intent nextintent = new Intent(BluetoothOnActivity.this, AddressActivity.class);
-                    startActivity(nextintent);
-                    overridePendingTransition(R.anim.fadein, R.anim.fadeout);
                 }
             }
         });
